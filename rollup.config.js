921b1e9952453597c0ef9e92f97a755c8ed569ce
Graham Pullan <gp10006@cam.ac.uk>
2017-12-28 10:42:15 +0000

    rollup with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,14 @@
 import babel from 'rollup-plugin-babel';
-//import uglify from 'rollup-plugin-uglify';
 
 export default {
 	input: 'src/Dbslice.js',
-	output: [ 
-	//{
-	//	file: 'build/dbslice.js',
-	//	format: 'iife',
-	//	name: 'dbslice'
-	//},
-	{	file: 'build/dbslice.js',
+	output: {
+		file: 'build/dbslice.js',
 		format: 'iife',
 		name: 'dbslice',
 		plugins: [
 		  babel({exclude:'node_modules/**'})
-		  //,uglify()
 		]
-	}]
+	}
 };
+
