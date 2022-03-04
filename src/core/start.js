1f850dc3fd8e49a6913f33aba092b01015eb27a7
Graham Pullan <gp10006@cam.ac.uk>
2022-03-04 11:10:57 +0000

    dbslice.start initial commit

---
+++ src/core/start.js
@@ -0,0 +1,32 @@
+import { update } from './update.js';
+import { dbsliceData } from './dbsliceData.js';
+import { makeSessionHeader } from './makeSessionHeader.js';
+import * as d3 from 'd3';
+
+function start( elementId, session ) {
+
+    if ( typeof(session) == 'string' ) {
+        console.log("session is a string");
+
+        // fetch session file
+
+        // then fetch metadata file (if not object
+        )
+
+
+
+    }
+
+    if ( typeof(session) == 'object' ) {
+        console.log("session is an object");
+
+        // header
+
+        //
+
+
+    }
+
+}
+
+export { start };
