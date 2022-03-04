d54ff99b09f76686534fb2ea56e02c5c751b98b1
Graham Pullan <gp10006@cam.ac.uk>
2022-03-04 18:19:27 +0000

    async fetch for session and metaData files

--- src/core/start.js
+++ src/core/start.js
@@ -1,31 +1,35 @@
 import { update } from './update.js';
+import { cfInit } from './cfInit.js';
 import { dbsliceData } from './dbsliceData.js';
 import { makeSessionHeader } from './makeSessionHeader.js';
 import * as d3 from 'd3';
 
-function start( elementId, session ) {
+async function start( elementId, session ) {
 
     if ( typeof(session) == 'string' ) {
         console.log("session is a string");
-
-        // fetch session file
-
-        // then fetch metadata file (if not object
-        )
-
-
-
+        let sessionUrl = session;
+        let response = await fetch(sessionUrl);
+        session = await response.json();
     }
 
-    if ( typeof(session) == 'object' ) {
-        console.log("session is an object");
-
-        // header
-
-        //
-
-
+    if ( typeof(session.metaData) == 'string') {
+        console.log("metaData is a string");
+        let metaDataUrl = session.metaData;
+        let response = await fetch(metaDataUrl);
+        session.metaData = await response.json();
     }
+
+    session.cfData = cfInit( session.metaData );
+
+    let element = d3.select( "#" + elementId );
+	let sessionHeader = element.select(".sessionHeader");
+    if ( sessionHeader.empty() ) makeSessionHeader( element, session.title, session.subtitle, session.config );
+
+    dbsliceData.session = session;
+	dbsliceData.elementId = elementId;
+
+
 
 }
 
