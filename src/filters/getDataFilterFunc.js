33205dede9e265ce16f2ade179e462b23f74d8e8
Graham Pullan <gp10006@cam.ac.uk>
2022-11-26 13:51:42 +0000

    modified line series to work with time varying data and sync

--- src/filters/getDataFilterFunc.js
+++ src/filters/getDataFilterFunc.js
@@ -1,14 +1,16 @@
 import { threeMeshFromStruct } from './threeMeshFromStruct.js';
 import { lineSeriesFromLines } from './lineSeriesFromLines.js';
 import { lineSeriesFromCsv } from './lineSeriesFromCsv.js';
+import { lineSeriesFromTimeSnapshotCsv } from './lineSeriesFromTimeSnapshotCsv.js'
 
 function getDataFilterFunc(dataFilterType) {
 
     const lookup = {
 
-        'threeMeshFromStruct'     : threeMeshFromStruct,
-        'lineSeriesFromLines'     : lineSeriesFromLines,
-        'lineSeriesFromCsv'       : lineSeriesFromCsv
+        'threeMeshFromStruct'           : threeMeshFromStruct,
+        'lineSeriesFromLines'           : lineSeriesFromLines,
+        'lineSeriesFromCsv'             : lineSeriesFromCsv,
+        'lineSeriesFromTimeSnapshotCsv' : lineSeriesFromTimeSnapshotCsv
         
     };
     
