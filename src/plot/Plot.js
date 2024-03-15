0fbf055f99628274738ba5e11aad709b8f29291b
Graham Pullan <gp10006@cam.ac.uk>
2024-03-15 09:56:50 +0000

    initial shot at integrating board-box

---
+++ src/plot/Plot.js
@@ -0,0 +1,28 @@
+import { Component } from 'board-box';
+
+//
+// Plot is a class that extends Component. It is a base class for all plot types.
+// It should provide the following methods:
+// - plot titles
+// - axis scales
+// - colorbar
+
+class Plot extends Component {
+    constructor(options) {
+        if (!options) { options={} }
+        this.containerClassName = options.containerClassName || "plot-area";
+        super(options);
+        this.layout = options.layout;
+        this.data = options.data;
+        this.fetchData = options.fetchData;
+        this.lastWidth = this.width;
+        this.lastHeight = this.height;
+        this.newData = true;
+    }
+
+    get checkResize() {
+        return (this.lastWidth !== this.width || this.lastHeight !== this.height);
+    }
+}
+
+export { Plot };
